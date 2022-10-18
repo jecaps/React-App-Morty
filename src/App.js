1e97e819fec096a49541e46ef2402dafa1344747
Jerika Kanz <76426580+jecaps@users.noreply.github.com>
2022-10-18 16:00:38 +0200

    Fetch data from API and passed properties to Card Component (#11)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
+
 import styled from "styled-components";
+
+import Header from "./components/Header";
 import Card from "./components/Card";
 import Nav from "./components/Nav";
-import Header from "./components/Header";
 
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const RES = await fetch("https://rickandmortyapi.com/api/character");
+      const DATA = await RES.json();
+      setCharacters(DATA.results);
+    }
+    getData();
+  }, []);
+
   return (
     <>
       <Header />
       <Main>
-        <Card />
+        <CardsList>
+          {characters.map((character) => (
+            <Card
+              key={character.id}
+              name={character.name}
+              image={character.image}
+            />
+          ))}
+        </CardsList>
       </Main>
       <Nav />
     </>
@@ -18,4 +40,10 @@ export default function App() {
 const Main = styled.main`
   margin: 5rem auto;
   width: 500px;
+`;
+
+const CardsList = styled.ul`
+  padding: 0;
+  gap: 5px;
+  list-style: none;
 `;
